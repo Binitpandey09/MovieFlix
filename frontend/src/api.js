b1a9b5041ca1d0f8b84b6995a3cf5c6412742611
vinitpandey09 <vinitpandey858@gmail.com>
2025-09-10 01:07:42 +0530

    Update changes

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// Create an Axios instance.
-// In a Create React App project, environment variables from a .env file
-// that start with REACT_APP_ are automatically loaded onto the process.env object.
-// You DO NOT need to import or configure the 'dotenv' package in the frontend.
+// Create an Axios instance with a base URL pointing to your Render backend.
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL: 'https://movieflix-sp5q.onrender.com'
 });
 
 export default api;
+
